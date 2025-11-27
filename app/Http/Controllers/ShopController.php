bc85ff99bc8ec789e566a611477aee2dcabfb4d7
Veljko <kanjevacveljko@gmail.com>
2025-11-27 03:22:58 +0100

    Load products from database and display them on shop page

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -2,15 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ProductModel;
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
     public function index()
     {
-        $products = [
-            "Iphone X", "Samsung A30", "Samsung A52s", "Iphone 16 pro",
-        ];
+        $products = ProductModel::all();
 
         return view('shop', compact('products'));
     }
--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -6,12 +6,37 @@
 
 @section("content")
 
-    @foreach($products as $product)
-        @if(in_array($product, ["Iphone X", "Iphone 16 pro"]))
-            <p>{{$product}} - SUPER SNIZENJE</p>
-        @else
-            <p>{{$product}}</p>
-        @endif
-    @endforeach
+    <div class="container mt-5">
+
+        <h2 class="mb-4">Products</h2>
+
+        <table class="table table-bordered table-striped">
+            <thead class="table-dark">
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Amount</th>
+                <th>Price</th>
+                <th>Image</th>
+            </tr>
+            </thead>
+
+            <tbody>
+            @foreach($products as $product)
+                <tr>
+                    <td>{{$product->id}}</td>
+                    <td>{{$product->name}}</td>
+                    <td>{{$product->description}}</td>
+                    <td>{{$product->amount}}</td>
+                    <td>{{$product->price}}</td>
+                    <td>{{$product->image}}</td>
+                </tr>
+            @endforeach
+            </tbody>
+
+        </table>
+
+    </div>
 
 @endsection
